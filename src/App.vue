68f42ea5d3aefafd4f5425202af090f4507d0211
Nuthana Gangadharappa <107018@tfcorp.com>
2022-09-23 22:12:45 +0530

    my vuethird commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <comp-a />
+</template>
+
+<script>
+import CompA from "./components/CompA.vue";
+
+export default {
+  name: "App",
+  components: {
+    CompA,
+  },
+  data() {
+    return {
+      userName: "Raju Rastogi",
+    };
+  },
+  methods: {
+    oneFunc() {
+      console.log("sending one function");
+    },
+  },
+  provide() {
+    return {
+      userName: this.userName,
+      // or userName: "Raju Rastogi",
+      someFuc: this.oneFunc,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CompC.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Component C</h1>
+  <h2>user Name by Provide_inject:{{ userName }}</h2>
+  <button @click="someFuc">click me</button>
+</template>
+
+<script>
+export default {
+  inject: ["userName", "someFuc"],
+};
+</script>
+
+<style></style>
